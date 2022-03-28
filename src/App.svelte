64c5096fd1d104ca1c14de932232451899b179c9
timoyo93 <37409999+timoyo93@users.noreply.github.com>
2022-03-28 14:41:18 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <div class="flex h-[100vh] justify-center items-center">
+    <div class="text-center">
+      <h1 class="text-4xl text-red-500">Hello {name}!</h1>
+      <p>
+        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+        how to build Svelte apps.
+      </p>
+    </div>
+  </div>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
